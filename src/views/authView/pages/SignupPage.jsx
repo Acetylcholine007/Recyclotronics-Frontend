a398781c179e59a0f073711c3d57b0f4adc18821
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-04-05 17:36:16 +0800

    Style adjustment in login/signup screen.

--- src/views/authView/pages/SignupPage.jsx
+++ src/views/authView/pages/SignupPage.jsx
@@ -6,10 +6,15 @@ import {
   Stack,
   TextField,
   Typography,
+  InputAdornment,
 } from "@mui/material";
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import AuthAPI from "../../../shared/apis/AuthAPI";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import LockIcon from '@mui/icons-material/Lock';
+import background from "../../../assets/BG.png";
 
 const SignupPage = () => {
   const [fullname, setFullname] = useState("");
@@ -25,7 +30,7 @@ const SignupPage = () => {
   return (
     <Box>
       <Container align="center" sx={{ marginTop: "4rem" }}>
-        <Typography variant="h3">Sign up for an account</Typography>
+        <Typography variant="h3" sx={{ marginBottom: "1.5rem" }}>Sign up for an account</Typography>
         <Stack component="form" spacing={2} noValidate autoComplete="off">
           <TextField
             id="fullname"
@@ -35,6 +40,14 @@ const SignupPage = () => {
             onChange={(e) => setFullname(e.target.value)}
             value={fullname}
             fullWidth={true}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircleIcon />
+                </InputAdornment>
+              ),
+            }}
+            sx={{backgroundColor: "#f1effb"}}
           />
           <TextField
             id="email"
@@ -44,6 +57,14 @@ const SignupPage = () => {
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             fullWidth={true}
+             InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AlternateEmailIcon />
+                </InputAdornment>
+              ),
+            }}
+            sx={{backgroundColor: "#f1effb"}}
           />
           <TextField
             id="password"
@@ -53,18 +74,27 @@ const SignupPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             fullWidth={true}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+            }}
+            sx={{backgroundColor: "#f1effb"}}
           />
-          <Button variant="contained" onClick={signupHandler}>
+          <Button variant="contained" size="large" onClick={signupHandler}>
             SIGNUP
           </Button>
           <Stack direction="row" justifyContent="center">
-            <Typography variant="h6">Already have an account?</Typography>
+            <Typography variant="h6" color="#7d7d7f">Already have an account?</Typography>
             <Button variant="text" onClick={() => history.push("/login")}>
-              Log In
+            <Typography color="#07b464" fontWeight="bold">Log In</Typography>
             </Button>
           </Stack>
         </Stack>
       </Container>
+      <img src={background} alt="Background image" style={{ width: "100%", transform: "translateY(65px)"}}/>
     </Box>
   );
 };
